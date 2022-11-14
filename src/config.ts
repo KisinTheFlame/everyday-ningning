c203223de75cc8e8be3cd8e76b15a7728ecfcd98
KisinTheFlame <870853294@qq.com>
2022-11-14 15:13:39 +0800

    feat: change resource url.

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ export const config: Config = {
     mode: process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : "development",
     sandbox: process.env.NODE_ENV === "development",
     backendPrefix: "https://ning.kisin.tech/api",
-    resourcePrefix: "https://ning-1304012538.file.myqcloud.com",
+    resourcePrefix: "https://ning.kisin.tech/api",
     postChannel: "3370836",
     maxTimeToTry: 10,
     excludedUsers: new Set<string>(),
